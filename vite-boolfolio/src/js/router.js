ef6af7aaa2b6aada574a5e11605f142574b40042
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-11-19 00:11:57 +0100

    added route for Home in router.js

--- vite-boolfolio/src/js/router.js
+++ vite-boolfolio/src/js/router.js
@@ -1,9 +1,11 @@
 import { createWebHistory, createRouter } from "vue-router";
 
 import AppProjects from "../pages/AppProjects.vue";
+import Home from "../pages/Home.vue";
 
 const routes = [
     { path: "/projects", component: AppProjects, name: "projects" },
+    { path: "/", component: Home, name: "home" },
 ]
 
 const router = createRouter({
