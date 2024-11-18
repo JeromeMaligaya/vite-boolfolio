d01a2f702291ef4c999a3d428c30604082dfb05b
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-11-18 17:34:14 +0100

    created ProjectsList.vue in pages folder

---
+++ vite-boolfolio/src/pages/AppProjects.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  name: "AppProjects",
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+    
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<template>
+    <main>
+
+    </main>
+</template>
+
+<style lang="scss" scoped>
+  
+</style>
