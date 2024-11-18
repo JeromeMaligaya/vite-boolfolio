806a0a075a2b339e7bc998cafb0f99932250a774
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-11-18 23:35:56 +0100

    added by props projectObj and filled template for #project-card in ProjectListItem

--- vite-boolfolio/src/components/ProjectsListItem.vue
+++ vite-boolfolio/src/components/ProjectsListItem.vue
@@ -1,6 +1,6 @@
 <script>
 export default {
-  name: "ProjectsListItem",
+  name: "projectsListItem",
   data() {
     return {
       
@@ -9,14 +9,54 @@ export default {
   components: {
     
   },
+  props: {
+    projectObj: {
+        required: true,
+        type: Object,
+    }
+  },
   methods: {
     
   },
+  created(){
+    console.log(this.projectObj);
+
+  }
 };
 </script>
 
 <template>
-  
+    <li class="col-6 g-3">
+        <div id="project-card" class="card">
+            <div class="card-body">
+                <div class="mb-2">
+                    <h5 class="mb-1">Name projectObj: </h5>
+                    <p class="card-text fw-bolder">
+                        {{ projectObj.name }}
+                    </p>
+                </div>
+                <div class="mb-2">
+                    <h5 class="mb-1">Started on: </h5>
+                    <p class="card-text fw-bolder">
+                        {{ projectObj.date }}
+                    </p>
+                </div>
+                <div class="mb-2">
+                    <h5 class="mb-1">Type: </h5>
+                    <p class="card-text fw-bolder">
+                        {{ projectObj.type.name}}
+                    </p>
+                </div>
+                
+                <div class="mb-2">
+                    <h5 class="mb-1">Description:</h5>
+                    <p class="card-text fw-bolder">
+                        {{ projectObj.description}}
+                    </p>
+                </div>
+            </div>
+        </div>
+    </li>
 </template>
 
 <style lang="scss" scoped>
