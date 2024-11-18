938e158f1985a66be42ac2f34e2dca627a6461c2
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-11-18 18:04:56 +0100

    created ProjectsListItem.vue in components folder

---
+++ vite-boolfolio/src/components/ProjectsListItem.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: "ProjectsListItem",
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+    
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss" scoped>
+  
+</style>
