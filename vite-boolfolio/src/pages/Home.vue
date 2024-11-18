a3648ecb59966a912654ff0c1e8fdaef1a850943
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-11-19 00:10:27 +0100

    created page Home.vue

---
+++ vite-boolfolio/src/pages/Home.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name:"Home",
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+    
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<template>
+    <main>
+        <div class="d-flex">
+            <h1 class="m-auto pt-5">WELCOME BACK! :D</h1>
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+  
+</style>
