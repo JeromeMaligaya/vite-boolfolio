544a88d5f49eb86947b1854b95b0b1c4fb59805c
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-11-18 17:29:49 +0100

    created ProjectsList.vue in components folder and imported axios

---
+++ vite-boolfolio/src/components/ProjectsList.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+    
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss">
+  
+</style>
