1d726c6dd09a7fe1fbd5b08bf202fd676d825636
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-11-19 00:12:48 +0100

    added Home in navbar of AppHeader

--- vite-boolfolio/src/components/AppHeader.vue
+++ vite-boolfolio/src/components/AppHeader.vue
@@ -17,16 +17,19 @@ export default {
 
 <template>
     <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar navbar-expand bg-success">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Vite-boolfolio</a>
+                <a class="navbar-brand fw-bolder text-warning" href="#">Vite-boolfolio</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <router-link class="nav-link active" aria-current="page" :to="{ 'name' : 'projects'}">Projects</router-link>
+                        <router-link class="nav-link active text-white" aria-current="page" :to="{ 'name' : 'home'}">Home</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link active text-white" aria-current="page" :to="{ 'name' : 'projects'}">Projects</router-link>
                     </li>
                 </ul>
                 </div>
