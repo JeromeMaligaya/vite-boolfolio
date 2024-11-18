252e55843779b7bde03a468e9811a6db6da5d9ab
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-11-18 18:02:29 +0100

    added by axios getProjects() in component ProjectList.vue

--- vite-boolfolio/src/components/ProjectsList.vue
+++ vite-boolfolio/src/components/ProjectsList.vue
@@ -2,24 +2,39 @@
 import axios from 'axios';
 
 export default {
-  data() {
+  name: "ProjectsLIst",
+  data() { 
     return {
-      
+      apiUrl:"http://127.0.0.1:8000/api/projects",
+      projectsList: [],
     };
   },
   components: {
     
   },
   methods: {
-    
+    getProjects(){
+        axios.get(this.apiUrl)
+            .then(response => {
+                console.log(response);
+                this.projectsList = response.data.results
+            })
+            .catch(function(error) {
+            console.log(error);
+            });
+
+    }
   },
+  created() {
+    this.getProjects();
+  }
 };
 </script>
 
 <template>
-  
+
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
   
 </style>
