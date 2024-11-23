b69bc898306f76b1346bd11afa9268401be1b98e
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-11-24 00:12:36 +0100

    created new page AppSingleProject

---
+++ vite-boolfolio/src/pages/AppSingleProject.vue
@@ -0,0 +1,28 @@
+<script>
+import ProjectsListItem from '../components/ProjectsListItem.vue';
+import AppLoader from '../components/AppLoader.vue';
+
+export default {
+  name:"AppSingleProject",  
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+    ProjectsListItem,
+    AppLoader,
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss" scoped>
+  
+</style>
