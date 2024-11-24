eaf4ef9edaf5057245d33aa38b15de76b8f46e10
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-11-24 01:02:32 +0100

    imported axios and added getSingleProject() in AppSingleProject

--- vite-boolfolio/src/pages/AppSingleProject.vue
+++ vite-boolfolio/src/pages/AppSingleProject.vue
@@ -2,11 +2,14 @@
 import ProjectsListItem from '../components/ProjectsListItem.vue';
 import AppLoader from '../components/AppLoader.vue';
 
+import axios from 'axios';
+
 export default {
   name:"AppSingleProject",  
   data() {
     return {
-      
+      apiUrl: "http://127.0.0.1:8000/api/projects",
+      singleProject: null,
     };
   },
   components: {
@@ -14,8 +17,20 @@ export default {
     AppLoader,
   },
   methods: {
-    
+    getSingleProject(){
+        axios.get(`${this.apiUrl}/${this.$route.params.id}`)
+        .then(response => {
+          console.log(response.data.result)
+          this.singleProject = response.data.result;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
   },
+  created(){
+    this.getSingleProject();
+  }
 };
 </script>
 
