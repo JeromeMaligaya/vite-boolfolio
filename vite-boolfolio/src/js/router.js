e9067fc3a689fd46383459a8fb1f58856216dcde
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-11-24 01:05:06 +0100

    added route for AppSingleProject in router.js

--- vite-boolfolio/src/js/router.js
+++ vite-boolfolio/src/js/router.js
@@ -2,10 +2,12 @@ import { createWebHistory, createRouter } from "vue-router";
 
 import AppProjects from "../pages/AppProjects.vue";
 import Home from "../pages/Home.vue";
+import AppSingleProject from "../pages/AppSingleProject.vue";
 
 const routes = [
     { path: "/projects", component: AppProjects, name: "projects" },
     { path: "/", component: Home, name: "home" },
+    { path: "/projects/:id", component: AppSingleProject, name: "projects.show" }
 ]
 
 const router = createRouter({
